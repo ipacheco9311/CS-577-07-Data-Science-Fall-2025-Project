d304d3e6ec653faeee5e7a9e5943465b76a887bd
Isaia Pacheco <ipacheco9311@sdsu.edu>
2025-11-16 16:32:56 -0800

    Code to fetch dataset

---
+++ data/dataset_id.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+from enum import IntEnum
+
+class DatasetIDs(IntEnum):
+    """
+    Enum for Dataset IDs in UCI's ML Repository .
+    """
+    Apartment_For_Rent_Classified = 555
