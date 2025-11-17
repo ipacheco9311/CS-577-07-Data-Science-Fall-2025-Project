2392ce84dd7e905599f9f48bb1f5870cf0e13874
Isaia Pacheco <ipacheco9311@sdsu.edu>
2025-11-16 22:03:45 -0800

    fix for intellisense

--- data/api.py
+++ data/api.py
@@ -1,23 +1,22 @@
 from __future__ import annotations
 from dotenv import dotenv_values
+from ucimlrepo import dotdict
 from ucimlrepo import fetch_ucirepo
 from enum import IntEnum
 import beaapi
+from pandas import DataFrame
 
 
 class UcIrvineAPI:
-
-    def __init__(self):
-        pass
-
     @staticmethod
-    def fetch_dataset(repo_id: int | None = None):
+    def fetch_dataset(repo_id: int | None = None) -> dotdict:
         """
         Loads a dataset from the UCI ML Repository, including the dataframes and metadata information
         :param repo_id: Dataset ID for UCI ML Repository
         :return: object containing dataset metadata, dataframes, and variable info in its properties
         """
         return fetch_ucirepo(id=repo_id)
+
 
 class UcIrvineDatasetIDs(IntEnum):
     """
@@ -27,17 +26,15 @@ class UcIrvineDatasetIDs(IntEnum):
 
 
 class BureauEconomicAnalysisAPI:
-
-    def __init__(self):
-        pass
-
     @staticmethod
-    def fetch_dataset(dataset_name, **kwargs):
+    def fetch_dataset(dataset_name, **kwargs) -> DataFrame:
         """
             Loads a dataset from the Bureau of Economic Analysis API, including the dataframes and metadata information
             :param dataset_name – Name of BEA dataset
             :param kwargs – Other named parameters as key-value pairs
-            :return: a dataframe containing real personal income of all U.S. states in 2019
+            :return:
+            A table with data. The 'DataValue' col will try to be converted to a numeric using pd.to_numeric().
+            '', '(NA)', and '(NM)'/'(D)' (when accompanied by the same in the NoteRef col) will be converted to np.nan.
          """
         user_id = dotenv_values()['BEA_KEY']
         return beaapi.get_data(user_id, dataset_name, **kwargs)
